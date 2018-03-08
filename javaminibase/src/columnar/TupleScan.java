e35087ce2abf94128070dc8b71d7df3df8ee329a
FPSLuozi <lawrenceyiranluo@gmail.com>
2018-03-07 20:01:40 -0700

    package

---
+++ javaminibase/src/columnar/TupleScan.java
@@ -0,0 +1,33 @@
+package heap;
+
+import global.TID;
+
+public class TupleScan {
+
+    /**
+     * Closes the TupleScan object
+     */
+    void closetuplescan(){
+        //todo
+    }
+
+    /**
+     * Retrieve the next tuple in a sequential scan
+     * @param tid
+     * @return
+     */
+    Tuple getNext(TID tid){
+        //todo
+        return null;
+    }
+
+    /**
+     * Position all scan cursors to the records with the given rids
+     * @param tid
+     * @return
+     */
+    boolean position(TID tid){
+        //todo
+        return false;
+    }
+}
