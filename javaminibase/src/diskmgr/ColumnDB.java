6c0912d3e504a6772ee9ed1dcc33b6a9edb509d4
Stephanie Bienz <sbienz@asu.edu>
2018-03-17 11:55:47 -0700

    New class ColumnDB created

---
+++ javaminibase/src/diskmgr/ColumnDB.java
@@ -0,0 +1,12 @@
+package diskmgr;
+
+import java.io.*;
+import bufmgr.*;
+import global.*;
+
+public class ColumnDB implements GlobalConst {
+    /**
+    Default contructor
+     **/
+    public ColumnDB() { }
+}//end of ColumnDB class
