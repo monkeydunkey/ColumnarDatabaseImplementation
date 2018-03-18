c48cf4c6bee19853d819fd86f7f021a7895e917a
jameskieley <jkieley@asu.edu>
2018-03-18 15:06:53 -0700

    add Bit Map file per instructions, with stubbed method

---
+++ javaminibase/src/bitmap/BM.java
@@ -0,0 +1,19 @@
+package bitmap;
+
+/*
+Bit Map File Implementation
+
+// columns value
+// rows record
+
+
+(see BT for analogy):
+
+ */
+public class BM {
+
+    public void printBitMap(BitMapHeaderPage header){
+        //todo
+        //For debug.
+    }
+}
