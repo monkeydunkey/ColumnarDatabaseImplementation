76710a4af781e2c0bdcb28bf9efb03c0c9694db4
FPSLuozi <lawrenceyiranluo@gmail.com>
2018-03-18 18:34:33 -0700

    Fix ValueClass files

---
+++ javaminibase/src/global/ValueIntClass.java
@@ -0,0 +1,8 @@
+package global;
+
+public class ValueIntClass extends ValueClass {
+	public int value = 0;
+	public ValueIntClass(int val) {
+		value = val;
+	}
+}
---
+++ javaminibase/src/global/ValueStrClass.java
@@ -0,0 +1,8 @@
+package global;
+
+public class ValueStrClass extends ValueClass {
+	public String value = "";
+	public ValueIntClass(String val) {
+		value = val;
+	}
+}
