d0fce71dcf24b1773c6f4852dbe241dac883bbdf
FPSLuozi <lawrenceyiranluo@gmail.com>
2018-03-18 18:34:51 -0700

    Fix ValueClass files p2

--- javaminibase/src/global/ValueClass.java
+++ javaminibase/src/global/ValueClass.java
@@ -5,16 +5,3 @@ public abstract class ValueClass extends java.lang.Object {
     }
 }
 
-public class ValueIntClass extends ValueClass {
-	public int value = 0;
-	public ValueIntClass(int val) {
-		value = val;
-	}
-}
-
-public class ValueStrClass extends ValueClass {
-	public String value = "";
-	public ValueIntClass(String val) {
-		value = val;
-	}
-}
