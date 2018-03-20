eb9bade7b893d8588cff2fd719e16a571cba31c5
Stephanie Bienz <sbienz@asu.edu>
2018-03-20 16:53:20 -0700

    Implementation of logic to extract information from the args array

--- javaminibase/src/tests/delete_query.java
+++ javaminibase/src/tests/delete_query.java
@@ -9,6 +9,8 @@ import btree.BTreeFile;
 import columnar.*;
 import java.io.*;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import diskmgr.pcounter;
 
@@ -17,9 +19,9 @@ public class delete_query extends TestDriver implements GlobalConst {
      * Command line invocation of:
      * delete_query COLUMNDBNAME COLUMNARFILENAME [TARGETCOLUMNNAMES] VALUECONSTRAINT NUMBUF ACCESSTYPE PURGED
      * COLUMNDBNAME and COLUMNARFILENAME are strings
-     * TARGETCOLUMNNAMES is an array of strings
+     * TARGETCOLUMNNAMES is an list of column names of the form "[TC1 TC2 ... TCN]"
      * VALUECONSTRAINT is of the form "{COLUMNNAME OPERATOR VALUE}"
-     * NUMBUF is an integer (MiniBase will use at most NUMBUF buffer pages to run the query
+     * NUMBUF is an integer (MiniBase will use at most NUMBUF buffer pages to run the query)
      * ACCESSTYPE is a string; valid inputs are "FILESCAN", "COLUMNSCAN", "BTREE", or "BITMAP"
      * PURGED is a boolean flag; 0 -> deleted tuples will not be purged from DB, 1 -> deleted tuples will be purged from DB
      */
@@ -32,14 +34,92 @@ public class delete_query extends TestDriver implements GlobalConst {
     public static void main( String[] args ) {
         pcounter.initialize(); // Initializes read & write counters to 0
 
+        String str = "";
+        String dbName = "";
+        String cfName = "";
+        String valConst_ColName = "";
+        String valConst_Operator = "";
+        String valConst_Value = "";
+        String numBuf = "";
+        String accessType = "";
+        String purged = "";
+        List<String> targetColNames = new ArrayList<>();
+        boolean colNamesDone = false;
+        int newIndex = 0;
+
+        for( int i = 0; i < args.length; i++ )
+        {
+            if( i == 0 ) // COLUMNDBNAME
+                dbName = args[i];
+            else if( i == 1 ) // COLUMNARFILENAME
+                cfName = args[i];
+            else if( args[i].charAt(0) == '[' ) // Start of TAGETCOLUMNNAMES list
+            {
+                str = args[i].substring(1);
+
+                if( args[i].charAt( args[i].length() - 1 ) == ']' ) // Covers case of only one element in TAGETCOLUMNNAMES list
+                {
+                    str = str.substring( 0, str.length() - 1 );
+                    targetColNames.add(str);
+                    colNamesDone = true;
+                    newIndex = i + 1; // Pointing to start of VALUECONSTRAINT
+                }
+                else // First element in TARGETCOLUMNNAMES list
+                {
+                    targetColNames.add(str);
+                }
+            }
+            else if( args[i].charAt( args[i].length() - 1 ) == ']' ) // Last element in TARGETCOLUMNNAMES list
+            {
+                    str = args[i].substring( 0, args[i].length() - 1 );
+                    targetColNames.add(str);
+                    colNamesDone = true;
+                    newIndex = i + 1; // Pointing to start of VALUECONSTRAINT
+            }
+            else if( colNamesDone == false ) // In the middle of the TARGETCOLUMNNAMES list
+            {
+                targetColNames.add(args[i]);
+            }
+            else if( i == newIndex ) // VALUECONSTRAINT -> COLUMNNAME
+            {
+                valConst_ColName = args[i].substring(1); // Removes { from COLUMNNAME
+            }
+            else if( i == newIndex + 1 ) // VALUECONSTRAINT -> OPERATOR
+            {
+                valConst_Operator = args[i];
+            }
+            else if( i == newIndex + 2 ) // VALUECONSTRAINT -> VALUE
+            {
+                valConst_Value = args[i].substring( 0, args[i].length() - 1 ); // Removes } from VALUE
+            }
+            else if( i == newIndex + 3 ) // NUMBUF
+            {
+                numBuf = args[i];
+            }
+            else if( i == newIndex + 4 ) // ACCESSTYPE
+            {
+                accessType = args[i];
+            }
+            else // PURGED
+            {
+                purged = args[i];
+            }
+        }
+
         /* Uncomment for debugging */
-        /* System.out.println( "COLUMNDBNAME: " + argv[0] ); // Name of DB previously inserted
-         * System.out.println( "COLUMNARFILE: " + argv[1] );
-         * System.out.println( "[TARGETCOLUMNNAMES]: " + argv[2] );
-         * System.out.println( "VALUECONSTRAINT: " + argv[3] );
-         * System.out.println( "NUMBUF: " + argv[4] );
-         * System.out.println( "ACCESSTYPE: " + argv[5] );
-         * System.out.println( "PURGED: " + argv[6] );
+        /* System.out.println("COLUMNDNAME: " + dbName);
+         * System.out.println("COLUMNARFILENAME: " + cfName);
+         * System.out.println("TARGETCOLUMNNAMES: ");
+         * for( int i = 0; i < targetColNames.size(); i++ )
+         * {
+         *   System.out.println(targetColNames.get(i));
+         * }
+         * System.out.println("VALUECONSTRAINT COLUMNNAME: " + valConst_ColName);
+         * System.out.println("VALUECONSTRAINT OPERATOR: " + valConst_Operator);
+         * System.out.println("VALUECONSTRAINT VALUE: " + valConst_Value);
+         * System.out.println("NUMBUF: " + numBuf);
+         * System.out.println("ACCESSTYPE: " + accessType);
+         * System.out.println("PURGED: " + purged);
          */
 
         System.out.println( "Running delete_query tests..." );
