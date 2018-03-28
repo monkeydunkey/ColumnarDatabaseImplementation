70a463e1052ce6a4856617a63c56862807a02594
James Kieley <jkieley@asu.edu>
2018-03-27 20:18:47 -0700

    just got bitmap read working

--- javaminibase/src/main/java/tests/BitMapTests.java
+++ javaminibase/src/main/java/tests/BitMapTests.java
@@ -1,15 +1,8 @@
 package tests;
 
 
-import btree.AddFileEntryException;
-import btree.ConstructPageException;
-import btree.GetFileEntryException;
 import columnar.Columnarfile;
-import global.SystemDefs;
-import heap.*;
 import org.junit.Test;
-
-import java.io.IOException;
 
 public class BitMapTests {
 
@@ -23,13 +16,4 @@ public class BitMapTests {
         Index.run(new String[]{ "column_db_name", "columnar_file_name", "C", "BITMAP"});
     }
 
-    @Test
-    public void test() throws Exception{
-        // // batchinsert DATAFILENAME COLUMNDBNAME COLUMNARFILENAME NUMCOLUMNS
-        String columnDbName = "column_db_name";
-        batchinsert.run(new String[] {"smalltest.txt", columnDbName,"columnar_file_name","4" });
-
-        Columnarfile columnarfile = new Columnarfile(columnDbName);
-//        columnarfile.createBitMapIndex()
-    }
 }
